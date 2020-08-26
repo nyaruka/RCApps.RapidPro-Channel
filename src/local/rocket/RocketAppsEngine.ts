5868c52b9ecf96ef5c83fcf966434f80facf8f11
paulobernardo <paulo.bernardo@ilhasoft.com.br>
2020-08-26 15:37:05 -0300

    added support to bot send livechat messages

--- src/local/rocket/RocketAppsEngine.ts
+++ src/local/rocket/RocketAppsEngine.ts
@@ -1,9 +1,10 @@
-import { IModify, IRead } from '@rocket.chat/apps-engine/definition/accessors';
+import { HttpStatusCode, IModify, IRead } from '@rocket.chat/apps-engine/definition/accessors';
 import { IVisitor } from '@rocket.chat/apps-engine/definition/livechat';
+import { IRoom, RoomType } from '@rocket.chat/apps-engine/definition/rooms';
 import { IUser } from '@rocket.chat/apps-engine/definition/users';
 
 import IRocketInternalDataSource from '../../data/rocket/IRocketInternalDataSource';
-import { RoomType, IRoom } from '@rocket.chat/apps-engine/definition/rooms';
+import AppError from '../../domain/AppError';
 
 export default class RocketAppsEngine implements IRocketInternalDataSource {
 
@@ -21,26 +22,43 @@ export default class RocketAppsEngine implements IRocketInternalDataSource {
     }
 
     public async sendLivechatMessage(botAgent: IUser, visitor: IVisitor, text?: string, attachments?: Array<string>): Promise<string> {
-        throw new Error('Method not implemented.');
+
+        const rooms = await this.read.getLivechatReader().getLivechatRooms(visitor);
+        if (!rooms) {
+            throw new AppError(`Could not find livechat room for visitor: ${visitor.token}`, HttpStatusCode.NOT_FOUND);
+        }
+
+        // TODO: check if index 0 will work on all situations
+        const livechatMessageBuilder = this.modify.getCreator().startLivechatMessage()
+            .setRoom(rooms[0])
+            .setSender(botAgent);
+
+        text && livechatMessageBuilder.setText(text);
+        // TODO: handle attachments
+        // attachments && livechatMessageBuilder.setAttachments(attachments);
+
+        return await this.modify.getCreator().finish(livechatMessageBuilder);
     }
 
     public async sendMessage(bot: IUser, userUsername: string, text?: string, attachments?: Array<string>): Promise<string> {
 
-        const messageBuilder = this.modify.getCreator().startMessage();
         let room: IRoom | undefined = await this.read.getRoomReader().getDirectByUsernames([bot.username, userUsername]);
 
         // if user have never spoken with the bot, the room is undefined, we need to create a new direct room
         if (!room) {
-            const roomBuilder = await this.modify.getCreator().startRoom();
-
-            roomBuilder.setCreator(bot).setType(RoomType.DIRECT_MESSAGE).setDisplayName('');
-            roomBuilder.setMembersToBeAddedByUsernames([userUsername]);
+            const roomBuilder = await this.modify.getCreator().startRoom()
+                .setCreator(bot)
+                .setType(RoomType.DIRECT_MESSAGE)
+                .setDisplayName('')
+                .setMembersToBeAddedByUsernames([userUsername]);
 
             const roomId = await this.modify.getCreator().finish(roomBuilder);
             room = await this.read.getRoomReader().getById(roomId);
         }
 
-        messageBuilder.setRoom(room!).setSender(bot);
+        const messageBuilder = this.modify.getCreator().startMessage()
+            .setRoom(room!)
+            .setSender(bot);
         text && messageBuilder.setText(text);
         // TODO: handle attachments
         // attachments && messageBuilder.setAttachments(attachments)
